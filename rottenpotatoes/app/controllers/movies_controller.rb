8345624cbc524e9ee82f43e3271a70af680e8b52
Ola Ajibode <ola.ajibode@gmail.com>
2015-12-01 00:01:53 +0000

    removed vendor dir

--- rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/app/controllers/movies_controller.rb
@@ -27,6 +27,8 @@ class MoviesController < ApplicationController
       redirect_to :sort => sort, :ratings => @selected_ratings and return
     end
     @movies = Movie.where(rating: @selected_ratings.keys).order(ordering)
+
+
   end
 
   def new
